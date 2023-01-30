a046f5647ac370b6aa80531980b88f05e8a1208c
JK NITHIN <jknithin36@gmail.com>
2023-01-30 11:46:12 +0530

    react frontend

---
+++ client/src/components/Profile.js
@@ -0,0 +1,127 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import avatar from "../assets/profile.png";
+import { Toaster } from "react-hot-toast";
+import { useFormik } from "formik";
+import { profileValidation } from "../helper/validate";
+import styles from "../styles/UserName.module.css";
+import extend from "../styles/Profile.module.css";
+import convertToBase64 from "../helper/convert";
+
+export default function Profile() {
+  const [file, setFile] = useState();
+  const formik = useFormik({
+    initialValues: {
+      fristname: "",
+      lastName: "",
+      email: "doyol56239@cnogs.com",
+      mobile: "",
+      address: "",
+    },
+    validate: profileValidation,
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit: async (values) => {
+      values = await Object.assign(values, { profile: file || "" });
+      // setUsername(values.username);
+      // navigate("/password");
+      console.log(values);
+    },
+  });
+
+  /** formik doensn't support file upload so we need to create this handler */
+  const onUpload = async (e) => {
+    const base64 = await convertToBase64(e.target.files[0]);
+    setFile(base64);
+  };
+
+  return (
+    <div className="container mx-auto">
+      <Toaster position="top-center" reverseOrder={false}></Toaster>
+
+      <div className="flex justify-center items-center h-screen">
+        <div
+          className={`${styles.r_glass} `}
+          style={{ width: "45%", paddingTop: "3em" }}
+        >
+          <div className="title flex flex-col items-center">
+            <h4 className="text-5xl font-bold">Profile</h4>
+            <span className="py-4 text-xl w-2/3 text-center text-gray-500">
+              You can Update the details
+            </span>
+          </div>
+
+          <form className="py-1" onSubmit={formik.handleSubmit}>
+            <div className="profile flex justify-center py-4">
+              <label htmlFor="profile">
+                <img
+                  src={file || avatar}
+                  className={`${styles.profile_img} ${extend.profile_img}`}
+                  alt="avatar"
+                />
+              </label>
+
+              <input
+                onChange={onUpload}
+                type="file"
+                id="profile"
+                name="profile"
+              />
+            </div>
+
+            <div className="textbox flex flex-col items-center gap-6">
+              <div className="name flex w-3/4 gap-10">
+                <input
+                  {...formik.getFieldProps("fristName")}
+                  className={`${styles.textbox} ${extend.textbox}`}
+                  type="text"
+                  placeholder="FristName"
+                />
+                <input
+                  {...formik.getFieldProps("lastName")}
+                  className={`${styles.textbox} ${extend.textbox}`}
+                  type="text"
+                  placeholder="LastName"
+                />
+              </div>
+              <div className="name flex w-3/4 gap-10">
+                <input
+                  {...formik.getFieldProps("mobile")}
+                  className={`${styles.textbox} ${extend.textbox}`}
+                  type="text"
+                  placeholder="Mobile No."
+                />
+                <input
+                  {...formik.getFieldProps("email")}
+                  className={`${styles.textbox} ${extend.textbox}`}
+                  type="text"
+                  placeholder="Email*"
+                />
+              </div>
+
+              <input
+                {...formik.getFieldProps("address")}
+                className={`${styles.textbox} ${extend.textbox}`}
+                type="text"
+                placeholder="Address"
+              />
+
+              <button className={styles.btn} type="submit">
+                Update
+              </button>
+            </div>
+
+            <div className="text-center py-4">
+              <span className="text-gray-500">
+                come back later?{" "}
+                <Link className="text-red-500" to="/">
+                  LogOut
+                </Link>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
